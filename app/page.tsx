1bd218789570a5558b8c73ce7fe2f1ec42676191
mitori <i@mitori.me>
2025-05-02 23:01:35 +0900

    fixed OGP info

--- app/page.tsx
+++ app/page.tsx
@@ -3,33 +3,6 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 
-export const metadata = {
-  title: '青空文庫をTwitterっぽく表示するやつ',
-  description: '青空文庫の作品をTwitterのスレッド風に表示する非公式ビューア。ツイ廃向け。',
-  openGraph: {
-    title: '青空文庫をTwitterっぽく表示するやつ',
-    description: '青空文庫の作品をTwitterのスレッド風に表示する非公式ビューア。ツイ廃向け。',
-    url: 'https://aozora-twi.vercel.app',
-    siteName: '青空文庫をTwitterっぽく表示するやつ',
-    images: [
-      {
-        url: 'https://lh3.googleusercontent.com/pw/AP1GczOgYDPBofbDogGE0NXKrpiO8vlC64WNQmoPN4EuDF-OZpkudiPnorfEGdSZBxz1g2Fith4_ArOEAAHWJegfkfX102MyYyqF9Ntfjt7my-xT3cgs_4VtylpgB02MfKDgYaAG_SoszlKmlYwiVbpALkRn=w1200-h630-s-no',
-        width: 1200,
-        height: 630,
-        alt: '青空文庫Twitter OGP',
-      },
-    ],
-    locale: 'ja_JP',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: '青空文庫をTwitterっぽく表示するやつ',
-    description: '青空文庫の作品をTwitterのスレッド風に表示する非公式ビューア。ツイ廃向け。',
-    images: ['https://lh3.googleusercontent.com/pw/AP1GczOgYDPBofbDogGE0NXKrpiO8vlC64WNQmoPN4EuDF-OZpkudiPnorfEGdSZBxz1g2Fith4_ArOEAAHWJegfkfX102MyYyqF9Ntfjt7my-xT3cgs_4VtylpgB02MfKDgYaAG_SoszlKmlYwiVbpALkRn=w1200-h630-s-no'],
-  },
-}
-
 export default function Home() {
   const [titleShorten, setTitleShorten] = useState('')
   const [showUserName, setShowUserName] = useState(false)
