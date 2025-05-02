88cbac62e78616aae7a0944c18f8fe8d93532fbe
mitori <i@mitori.me>
2025-05-02 22:57:56 +0900

    added OGP info

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,33 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 
+export const metadata = {
+  title: '青空文庫をTwitterっぽく表示するやつ',
+  description: '青空文庫の作品をTwitterのスレッド風に表示する非公式ビューア。ツイ廃向け。',
+  openGraph: {
+    title: '青空文庫をTwitterっぽく表示するやつ',
+    description: '青空文庫の作品をTwitterのスレッド風に表示する非公式ビューア。ツイ廃向け。',
+    url: 'https://aozora-twi.vercel.app',
+    siteName: '青空文庫をTwitterっぽく表示するやつ',
+    images: [
+      {
+        url: 'https://lh3.googleusercontent.com/pw/AP1GczOgYDPBofbDogGE0NXKrpiO8vlC64WNQmoPN4EuDF-OZpkudiPnorfEGdSZBxz1g2Fith4_ArOEAAHWJegfkfX102MyYyqF9Ntfjt7my-xT3cgs_4VtylpgB02MfKDgYaAG_SoszlKmlYwiVbpALkRn=w1200-h630-s-no',
+        width: 1200,
+        height: 630,
+        alt: '青空文庫Twitter OGP',
+      },
+    ],
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '青空文庫をTwitterっぽく表示するやつ',
+    description: '青空文庫の作品をTwitterのスレッド風に表示する非公式ビューア。ツイ廃向け。',
+    images: ['https://lh3.googleusercontent.com/pw/AP1GczOgYDPBofbDogGE0NXKrpiO8vlC64WNQmoPN4EuDF-OZpkudiPnorfEGdSZBxz1g2Fith4_ArOEAAHWJegfkfX102MyYyqF9Ntfjt7my-xT3cgs_4VtylpgB02MfKDgYaAG_SoszlKmlYwiVbpALkRn=w1200-h630-s-no'],
+  },
+}
+
 export default function Home() {
   const [titleShorten, setTitleShorten] = useState('')
   const [showUserName, setShowUserName] = useState(false)
@@ -31,14 +58,14 @@ export default function Home() {
         headers: { 'Content-Type': 'application/json' },
       })
       if (!res.ok) {
-        alert('検索に失敗しました。')
+        alert('検索に失敗しました。時間をおいて再度お試しいただくか、別な作品をお試しください。')
         return
       }
       const data = await res.json()
       setSearchResults(data.results || [])
       setShowResults(true)
     } catch (error) {
-      alert('検索中にエラーが発生しました。')
+      alert('検索中にエラーが発生しました。時間をおいて再度お試しください。')
     }
   }
 
@@ -53,7 +80,7 @@ export default function Home() {
     })
 
     if (!res.ok) {
-      alert('取得に失敗しました。')
+      alert('取得に失敗しました。別な作品をお試しください。')
       setShowResults(true)
       setWaitForLoading(false)
       return
